707be3d1e1b8930207706141e62c76b9e15912b1
oblivion4us <oblivion4us@icloud.com>
2024-11-04 21:56:27 +0200

    update index.html and add code for routes.py

---
+++ form_app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for
+from form_app import app
+
+cards =[]
+
+app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        city = request.form.get('city')
+        age = request.form.get('age')
+        hobby = request.form.get('hobby')
+
+        if name and city and age and hobby:
+            cards.append({'name': name,
+                          'city': city,
+                          'age': age,
+                          'hobby': hobby
+                          })
+            return redirect(url_for('index'))
+
+    return render_template('index.html', posts=cards)
